cb2376220c3c21150d59bfc07662ba30558d5e42
Lucas Rangel <lucas.cezimbra@acad.pucrs.br>
2014-10-22 13:53:15 -0200

    Partes 4, 5 e 6

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Main {
-    private static ArrayList<String> lista = new ArrayList<String>();
+    private static ArrayList<String> listaNomes = new ArrayList<String>();
 
     public static void main(String[] args){
 
@@ -10,21 +10,43 @@ public class Main {
     }
 
     public void adicionarPessoas(){
-        lista.add("Lucas");
-        lista.add("Henrique");
-        lista.add("Paula");
-        lista.add("Marcus");
-        lista.add("Julia");
+        listaNomes.add("Lucas");
+        listaNomes.add("Henrique");
+        listaNomes.add("Paula");
+        listaNomes.add("Marcus");
+        listaNomes.add("Julia");
     }
 
     public void removerPessoa(int i){
-        lista.remove(i);
+        listaNomes.remove(i);
     }
 
     public boolean estaNaLista(String nome){
-        return lista.contains(nome);
+        return listaNomes.contains(nome);
     }
 
+    public int tamanhoLista(){
+        return listaNomes.size();
+    }
 
+    public void soma (ArrayList<Integer> lista){
+        int total = 0;
+        for (int i = 0;i < lista.size(); i++){
+        total += lista.get(i);
+        }
+        System.out.println("Total da soma: " + total);
+    }
+
+    public ArrayList<Boolean> intArrayToBooleanArray (ArrayList<Integer> listaA, ArrayList<Integer> listaB){
+        ArrayList<Boolean> listaC = new ArrayList<Boolean>();
+        if (listaA.size() == listaB.size()){
+            for (int i = 0; i < listaA.size(); i++){
+                listaC.add (listaA.get(i) == listaB.get(i));
+            }
+        }
+        return listaC;
+    }
+
+    
 
 }
