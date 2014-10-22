317974275879249adc0b84f2a27c83846b704504
Lucas Rangel <lucas.cezimbra@acad.pucrs.br>
2014-10-22 14:24:47 -0200

    Testando exercicios

--- Main.java
+++ Main.java
@@ -4,12 +4,39 @@ public class Main {
     private static ArrayList<String> listaNomes = new ArrayList<String>();
 
     public static void main(String[] args){
+        adicionarPessoas();
+        System.out.println(listaNomes);
+        removerPessoa(2);
+        System.out.println(listaNomes);
+        System.out.println("Lucas está na lista? " + estaNaLista("Lucas"));
+        System.out.println("Tamanho da lista: " + tamanhoLista());
 
+        ArrayList<Integer> listaInteiros1 = new ArrayList<Integer>();
+        listaInteiros1.add(5);
+        listaInteiros1.add(3);
+        listaInteiros1.add(13);
+        listaInteiros1.add(10);
+        listaInteiros1.add(10);
+        System.out.println(listaInteiros1);
+        soma(listaInteiros1);
 
+        ArrayList<Integer> listaInteiros2 = new ArrayList<Integer>();
+        listaInteiros2.add(5);
+        listaInteiros2.add(19);
+        listaInteiros2.add(10);
+        listaInteiros2.add(5);
+        listaInteiros2.add(10);
+        System.out.println(listaInteiros2);
+
+        System.out.println(intArrayToBooleanArray(listaInteiros1, listaInteiros2));
+
+        System.out.println(interseccaoArrays(listaInteiros1, listaInteiros2));
+
+        System.out.println(uniaoArrays(listaInteiros1, listaInteiros2));
 
     }
 
-    public void adicionarPessoas(){
+    public static void adicionarPessoas(){
         listaNomes.add("Lucas");
         listaNomes.add("Henrique");
         listaNomes.add("Paula");
@@ -17,19 +44,19 @@ public class Main {
         listaNomes.add("Julia");
     }
 
-    public void removerPessoa(int i){
+    public static void removerPessoa(int i){
         listaNomes.remove(i);
     }
 
-    public boolean estaNaLista(String nome){
+    public static boolean estaNaLista(String nome){
         return listaNomes.contains(nome);
     }
 
-    public int tamanhoLista(){
+    public static int tamanhoLista(){
         return listaNomes.size();
     }
 
-    public void soma (ArrayList<Integer> lista){
+    public static void soma (ArrayList<Integer> lista){
         int total = 0;
         for (int i = 0;i < lista.size(); i++){
         total += lista.get(i);
@@ -37,7 +64,7 @@ public class Main {
         System.out.println("Total da soma: " + total);
     }
 
-    public ArrayList<Boolean> intArrayToBooleanArray (ArrayList<Integer> listaA, ArrayList<Integer> listaB){
+    public static ArrayList<Boolean> intArrayToBooleanArray (ArrayList<Integer> listaA, ArrayList<Integer> listaB){
         ArrayList<Boolean> listaC = new ArrayList<Boolean>();
         if (listaA.size() == listaB.size()){
             for (int i = 0; i < listaA.size(); i++){
@@ -47,17 +74,17 @@ public class Main {
         return listaC;
     }
 
-    public ArrayList<Integer> interseccaoArrays (ArrayList<Integer> listaA, ArrayList<Integer> listaB){
+    public static ArrayList<Integer> interseccaoArrays (ArrayList<Integer> listaA, ArrayList<Integer> listaB){
         ArrayList<Integer> listaC = new ArrayList<Integer>();
         for (int i = 0; i < listaA.size(); i++){
-            if (listaB.contains(listaA.get(i))){
+            if (listaB.contains(listaA.get(i)) && !listaC.contains(listaA.get(i))){
                 listaC.add(listaA.get(i));
             }
         }
         return listaC;
     }
 
-    public ArrayList<Integer> uniaoArrays (ArrayList<Integer> listaA, ArrayList<Integer> listaB){
+    public static ArrayList<Integer> uniaoArrays (ArrayList<Integer> listaA, ArrayList<Integer> listaB){
         ArrayList<Integer> listaC = new ArrayList<Integer>();
         listaC.addAll(listaA);
         for (int i = 0; i < listaB.size(); i++){
